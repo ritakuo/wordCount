9bf948475ba6279bbf530faca9d54db5f4079c76
rita <ritakuo@fico.com>
2019-01-27 19:44:38 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Use AWS EMR to run a custom ( wordcount) jar
+
+## tl&dr
+- I can't find  sample wordcount jar to test out AWS EMR , so I create one. 
+
+- This program contains a standard mapreduce classes. ![edureka](https://www.edureka.co/blog/mapreduce-tutorial/) explains the mapreduce process.
+![](https://s3-us-west-2.amazonaws.com/donot-delete-github-image/Screen+Shot+2019-01-27+at+12.20.20+PM.png)
+
+## Pre-req
+
+### Compile the jar
+1. clone this repo
+2. maven clean, maven install 
+
+## run on AWS EMR
+1. Create a EC2 keypair PEM file to used for EMR
+2. Create a S3 bucket 
+3. Upload the jar in this repo to your s3 bucket ( You can make change and compile your own as well)
+4. Upload the input file to the same s3 bucket 
+5. Create a EMR cluster, choose EMR version that uses hadoop version 2.7.3 (to use a different hadoop version, change the pom.xml)
+6. After EMR provision finish, add a step for custom jar
+- for JAR location, point to the jar in the s3 bucket
+- for argument: s3://<your-bucket>/<your-input-file>.txt s3://<your-bucket>/out
+7. check output on s3 folder
+
---
+++ src/main/java/wordCount.java
@@ -0,0 +1,76 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import static org.apache.hadoop.metrics2.impl.MsInfo.Context;
+
+/**
+ * Created by ritakuo on 1/23/19.
+ * https://www.youtube.com/watch?v=G6kQ14AAzXQ
+ *
+ */
+public class wordCount {
+    //MR infra will call this class and pass in text
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(value.toString());
+            Text wordOut = new Text();
+            IntWritable one = new IntWritable(1);
+            while(st.hasMoreTokens()){
+                wordOut.set(st.nextToken());
+                context.write(wordOut, one);
+            }
+
+        }
+    }
+    public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text term, Iterable<IntWritable> ones, Context context)throws IOException, InterruptedException {
+            int count = 0;
+            Iterator<IntWritable> iterator = ones.iterator();
+            while(iterator.hasNext()){
+                count ++;
+                iterator.next();
+            }
+            IntWritable output = new IntWritable (count);
+            context.write(term,  output);
+        }
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        String[]  otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        if(otherArgs.length !=2){
+            System.err.println("Usage: WordCount <input_file> <output_directory>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "Word Count");
+        job.setJarByClass(wordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(SumReducer.class);
+        job.setNumReduceTasks(3);//will output to 10 files
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        boolean status = job.waitForCompletion(true);
+        if (status){
+            System.exit(0);
+        }else{
+            System.exit(1);
+        }
+
+    }
+}
